20f29b1d0c22df54fd31dd27d2c986a351621463
Daniel Beardsley <daniel@ifixit.com>
2015-05-06 00:47:05 -0700

    bin: add option parsing and help

--- nagnagnag.rb
+++ nagnagnag.rb
@@ -5,22 +5,55 @@ require './github.rb'
 
 class Nagnagnag 
    def initialize
+      @options = parse_options
       @client = Github.api
    end
 
    def parse_options
+      options = {
+         :repo => nil,
+         :no_activity_days => 30,
+         :close_after_days => 30,
+         :exempt_label => "stay open"
+      }
+
       @options = OptionParser.new do |opts|
          opts.banner = "Usage: nagnagnag --repo=user/repo"
        
          opts.on("-r", "--repo [REPO]", "github  username/repository") do |v|
             options[:repo] = v.split('/')
          end
+
+         opts.on("--no-activity-days=DAYS", OptionParser::DecimalInteger,
+                       "Number of days to wait after the last activity",
+                       "on an issue before commenting.") do |v|
+            options[:no_activity_days] = v
+         end
+
+         opts.on("--close-after-days=DAYS", OptionParser::DecimalInteger,
+                       "Number of days to wait after the last comment",
+                       "from this bot before closing an issue.") do |v|
+            options[:close_after_days] = v
+         end
+
+         opts.on("--exempt-label=LABEL", String,
+                       "Name of issue label that will prevent issues",
+                       "from being examined or modified by this bot.") do |v|
+            options[:close_after_days] = v
+         end
+
+         opts.on_tail("-h", "--help", "Show this message") do
+            puts opts
+            exit
+         end
+
+         if options[:repo].nil? 
+            puts opts
+            exit 1
+         end
       end.parse!
+      options
    end
 end
 
-class Options
-   def initialze
-   end
-end
-
+Nagnagnag.new
